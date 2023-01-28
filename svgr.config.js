8ce52bf211692793c765bedec17d4f243bed3750
iamakbarsha1 <iamakbarsha1@gmail.com>
2023-01-28 23:47:18 +0530

    Completed Phase 1 lovedicons

---
+++ svgr.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  //   configFile: "./svgr.config.js",
+  icon: true,
+  titleProp: true,
+  replaceAttrValues: { "#fff": "{props.color}" },
+  //   outDir: "figma-Icons",
+  //   index: true,
+  filenameCase: "pascal",
+  //   jsxRuntime: "automatic",
+};
